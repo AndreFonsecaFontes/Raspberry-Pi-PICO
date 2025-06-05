ae946f63e316b8ecc47e18a8ba6f152b8ce120e5
André <fonsecafontesandre@gmail.com>
2025-06-05 10:25:48 -0300

    changed the two main.c of project 2 and 3

--- project 3/potentiometer_gas.c
+++ project 3/potentiometer_gas.c
@@ -16,7 +16,7 @@
 #define ALARM_NUM 0
 #define ALARM_IRQ TIMER_IRQ_0
 
-#define DELAY 500000 // 1/Fs (in microseconds)
+#define DELAY 100000 // 1/Fs (in microseconds)
 
 #define GPIO_TENSAO 26
 #define ADC_TENSAO 0
@@ -26,34 +26,29 @@
 uint16_t contador = 0;
 
 uint8_t var = 0;
-uint8_t var_2 = 0;
-uint8_t var_3 = 0;
 
-unsigned char nome_1 [17] = "Romano Bibiano ";
-uint8_t nome_2 [17] = "Gabriel Baldo ";
-uint8_t nome_3 [17] = "Andre Fonseca ";
-unsigned char RA_1 [17] = "1600732221002";
-uint8_t RA_2 [17] = "1600732413028";
-uint8_t RA_3 [17] = "1600732413001";
+const uint8_t sensor[3] = {7,8,9};
+const uint16_t valor_pot_up [16] = {255,510,765,1020,1275,1530,1785,2040,2295,2550,2805,3060,3315,3570,3825,4080};
+uint8_t qnt = 0;
+uint8_t litros = 0;
 
-uint16_t pino_ch [8] = {16,15,14,13,12,11,10,9};
-uint8_t estado [8] = {0,0,0,0,0,0,0,0};
-uint16_t pos_cursor [8] = {0,2,4,6,8,10,12,14};
-uint16_t num_pinos [8] = {1,2,3,4,5,6,7,8};
-
-uint16_t leds_pin [6] = {17,18,19,20,21,22};
-
-uint8_t tensao = 0;
+uint8_t barra [8] = {
+  0b11111111,
+  0b11111111,
+  0b11111111,
+  0b11111111,
+  0b11111111,
+  0b11111111,
+  0b11111111,
+  0b11111111
+};
 
 //////////////////////////////////////////////////////////////////////////////
 
 void system_init(void);
 void alarm_irq() ;
 void button_callback(uint gpio);
-void nameOnLcd();
-void escolherLeds();
-void acenderLeds();
-void potPorcent();
+void mostraLcdGas ();
 
 //////////////////////////////////////////////////////////////////////////////
 
@@ -74,37 +69,15 @@ void alarm_irq(void)
   timer_hw->alarm[ALARM_NUM] = (uint32_t) target;
   
   contador++;
-  
-  //Leds Pisca Pisca
-
-  if(estado[0] == 1)
-  {
-    gpio_put(leds_pin[0], toggle);
-    toggle = !toggle;
-  }
-  else
-  {
-    gpio_put(leds_pin[0], OUT_L);
-  }
-
-
-  if(estado[1] == 1)
-  {
-    gpio_put(leds_pin[1], toggle_2);
-    toggle_2 = !toggle_2;
-  }
-  else
-  {
-    gpio_put(leds_pin[1], OUT_L);
-  }
-
-//////////////////////////////////////////////////////////////////////////////
 
 }
+
+//////////////////////////////////////////////////////////////////////////////
 
 int main() {
   stdio_init_all();
   system_init();
+  cria_caractere_lcd(0,barra);
   sleep_ms (5);
 
 
@@ -112,14 +85,11 @@ int main() {
   {
 //////////////////////////////////////////////////////////////////////////////
 
-    nameOnLcd();
-    escolherLeds();
-    acenderLeds();
-    potPorcent();
+  mostraLcdGas();
 
 //////////////////////////////////////////////////////////////////////////////
 
-    sleep_ms(1);
+    sleep_ms(5);
   
   }
 
@@ -166,7 +136,7 @@ void system_init(void)
   init_lcd();
   adc_init();
   adc_gpio_init(GPIO_TENSAO);
-  adc_select_input(ADC_TENSAO);
+  
 
   sleep_ms(5);
 
@@ -175,135 +145,35 @@ void system_init(void)
 
 //////////////////////////////////////////////////////////////////////////////
 
-void nameOnLcd (void)
+void mostraLcdGas (void)
 {
-    if(contador == 0 && var  == 0)
-  {
-    posicao_cursor_lcd(1,1);
-    escreve_frase_ram_lcd(nome_1);
-    posicao_cursor_lcd(2,0);
-    escreve_frase_ram_lcd(RA_1);
-    DesligaCursor();
+  uint16_t sensor_comb = 0;
 
-  }
-    else if (contador == 6 && var  == 0 )
+  adc_select_input(ADC_TENSAO);
+  adc_read();
+  sensor_comb = adc_read();
+
+  litros = (72*sensor_comb)/4095;
+
+
+  for(uint8_t i =0; i<16; i++)
   {
-    posicao_cursor_lcd(1,1);
-    escreve_frase_ram_lcd(nome_2);
-    posicao_cursor_lcd(2,0);
-    escreve_frase_ram_lcd(RA_2);
-    DesligaCursor();
+    if(sensor_comb >= valor_pot_up[i])
+    {
+      posicao_cursor_lcd(1, i);
+      escreve_caractere_lcd(0); 
+    }
   }
-    else if (contador == 12 && var  == 0 )
-  {
-    posicao_cursor_lcd(1,1);
-    escreve_frase_ram_lcd(nome_3); 
-    posicao_cursor_lcd(2,0);
-    escreve_frase_ram_lcd(RA_3);
-    DesligaCursor();
-  }
-    else if (contador == 18 && var  == 0)
+
+  if(litros != qnt)
   {
     LIMPA_DISPLAY();
-    var = 1;
-  }
-}
-
-//////////////////////////////////////////////////////////////////////////////
-
-void escolherLeds (void)
-{
-  if( var == 1)
-  {
-    for(uint8_t i=0; i<8; i++)
-    {
-      estado[i] = gpio_get(pino_ch[i]);
-      
-    }
-    for(uint8_t i = 0; i<8; i++)
-    {
-      posicao_cursor_lcd(1,pos_cursor[i]);
-      escreve_inteiro_lcd(estado[i]);
-    }
-  
-    DesligaCursor();
-  }
-}
-
-//////////////////////////////////////////////////////////////////////////////
-
-void acenderLeds (void)
-{
-  if((estado[2] && estado[3]) == 1)
-  {
-    gpio_put(leds_pin[2], OUT_H);
-  }
-  else
-  {
-    gpio_put(leds_pin[2], OUT_L);
+    qnt = litros;
   }
 
-  if(estado[4] == 1)
-  {
-    gpio_put(leds_pin[3], OUT_L);
-  }
-  else
-  {
-    gpio_put(leds_pin[3], OUT_H);
-  }
+  posicao_cursor_lcd(2, 0);
+  escreve_inteiro_lcd(litros);
+  escreve_frase_lcd("L");
 
-  if(estado[5] ==0 && estado[6] == 0 && estado[7] == 0)
-  {
-    gpio_put(leds_pin[4], OUT_H);
-  }
-  else 
-  {
-    gpio_put(leds_pin[4], OUT_L);
-  }
-}
-
-//////////////////////////////////////////////////////////////////////////////
-
-void potPorcent (void)
-{
-  if(var == 1)
-  {
-
-    adc_read();
-    uint16_t adc_reading = adc_read();
-    uint16_t porcent = (100 * adc_reading) / 4095;
-
-    //For pocent < 10
-    if(porcent >= 10 && var_3 == 0)
-    {
-      var_3 = 1;
-    }    
-    else if(porcent <10 && var_3 == 1)
-    {
-      if(contador)
-      {
-        LIMPA_DISPLAY();
-        var_3 = 0;
-      }
-    }
-
-    //For pocent == 100
-    if (porcent == 100 && var_2 == 0)
-    {
-      var_2 = 1;
-    }
-    else if(porcent < 100 && var_2 == 1)
-    { 
-      if(contador)
-      {
-        LIMPA_DISPLAY();
-        var_2 = 0;
-      }
-
-    }
-
-
-    posicao_cursor_lcd(2,0);
-    escreve_inteiro_lcd(porcent);
-  }
+  DesligaCursor();
 }
